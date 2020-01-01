86d14e50ff412abe10896d23b449f3935f67ce42
John Paul Helveston <john.helveston@gmail.com>
2020-01-01 12:53:23 -0500

    Added mousekeys support

--- keyboards/keebio-iris/jhelvy/rules.mk
+++ keyboards/keebio-iris/jhelvy/rules.mk
@@ -1,4 +1,5 @@
-AUTO_SHIFT_ENABLE = yes  # Autoshift by holding down a key
+AUTO_SHIFT_ENABLE = yes # Autoshift by holding down a key
+MOUSEKEY_ENABLE = yes
 ENCODER_ENABLE = yes
 EXTRAKEY_ENABLE = yes
 RGBLIGHT_ENABLE = no
